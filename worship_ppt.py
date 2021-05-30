a5c22d88c703838888d71b86810a16a0bdf9d6b4
osanskc <sumannam@gmail.com>
2021-05-30 15:06:37 +0900

    찬양 리스트 출력 추가

--- worship_ppt.py
+++ worship_ppt.py
@@ -3,8 +3,8 @@ import os
 import sys
 import shutil
 
-
-today_ppt_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\2021\\21.05.24"
+hymn_path = "E:\\git\\osanskc\\"
+today_ppt_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\2021\\21.05.31"
 
 # 예배 PPT
 ppt_origin_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\ogrinal_ppt"
@@ -15,16 +15,16 @@ worship_ppt_file4 = "2-1. 자막.pptx"
 
 # 주보 찬송가들
 hymn_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새찬송가"
-hymn_file1 = "001.ppt"
-hymn_file2 = "438.ppt"
-hymn_file3 = "536.ppt"
-hymn_file4 = "384.ppt"
-hymn_file5 = "569.ppt"
-hymn_file6 = "635.ppt"
+hymn_file1 = "001"
+hymn_file2 = "438"
+hymn_file3 = "536"
+hymn_file4 = "384"
+hymn_file5 = "569"
+hymn_file6 = "635"
 
 #교도문
 versicle_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새 교독문"
-versicle_file = "095.pptx"
+versicle_file = "095"
 
 
 def createFolder(directory):
@@ -34,29 +34,73 @@ def createFolder(directory):
     except OSError:
         print ('Error: Creating directory. ' +  directory)
 
-
-
 # 예배 폴더 생성
-createFolder(today_ppt_path)
-target_ppt_path = today_ppt_path + "\\ppt"
-createFolder(target_ppt_path)
+def createTargetFolder():
+    createFolder(today_ppt_path)
+    target_ppt_path = today_ppt_path + "\\ppt"
+    createFolder(target_ppt_path)
+    return target_ppt_path
 
 
-# 예배 PPT 파일 생성
-shutil.copy(ppt_origin_path + "\\" + worship_ppt_file1, today_ppt_path + "\\" + worship_ppt_file1)
-shutil.copy(ppt_origin_path + "\\" + worship_ppt_file2, today_ppt_path + "\\" + worship_ppt_file2)
-shutil.copy(ppt_origin_path + "\\" + worship_ppt_file3, today_ppt_path + "\\" + worship_ppt_file3)
-shutil.copy(ppt_origin_path + "\\" + worship_ppt_file4, today_ppt_path + "\\" + worship_ppt_file4)
+def copyFiles(target_ppt_path):
+    # 예배 PPT 파일 생성
+    shutil.copy(ppt_origin_path + "\\" + worship_ppt_file1, today_ppt_path + "\\" + worship_ppt_file1)
+    shutil.copy(ppt_origin_path + "\\" + worship_ppt_file2, today_ppt_path + "\\" + worship_ppt_file2)
+    shutil.copy(ppt_origin_path + "\\" + worship_ppt_file3, today_ppt_path + "\\" + worship_ppt_file3)
+    shutil.copy(ppt_origin_path + "\\" + worship_ppt_file4, today_ppt_path + "\\" + worship_ppt_file4)
+
+    hymn_file_ppt1 = hymn_file1 + ".ppt"
+    hymn_file_ppt2 = hymn_file2 + ".ppt"
+    hymn_file_ppt3 = hymn_file3 + ".ppt"
+    hymn_file_ppt4 = hymn_file4 + ".ppt"
+    hymn_file_ppt5 = hymn_file5 + ".ppt"
+    hymn_file_ppt6 = hymn_file6 + ".ppt"
+
+    # 찬송가 파일 복사
+    shutil.copy(hymn_ppt_path + "\\" + hymn_file_ppt1, target_ppt_path + "\\" + hymn_file_ppt1)
+    shutil.copy(hymn_ppt_path + "\\" + hymn_file_ppt2, target_ppt_path + "\\" + hymn_file_ppt2)
+    shutil.copy(hymn_ppt_path + "\\" + hymn_file_ppt3, target_ppt_path + "\\" + hymn_file_ppt3)
+    shutil.copy(hymn_ppt_path + "\\" + hymn_file_ppt4, target_ppt_path + "\\" + hymn_file_ppt4)
+    shutil.copy(hymn_ppt_path + "\\" + hymn_file_ppt5, target_ppt_path + "\\" + hymn_file_ppt5)
+    shutil.copy(hymn_ppt_path + "\\" + hymn_file_ppt6, target_ppt_path + "\\" + hymn_file_ppt6)
+
+    # 교독문 파일 복사
+    versicle_file_pptx = versicle_file + ".pptx"
+    shutil.copy(versicle_ppt_path + "\\" + versicle_file_pptx, target_ppt_path + "\\" + versicle_file_pptx)
+
+# target_ppt_path = createTargetFolder()
+# copyFiles(target_ppt_path)
+
+hymn_file = hymn_path + "hymn_list.txt"
+hymn_list = open(hymn_file, 'r')
+
+hymn_queue = []
 
 
-# 찬송가 파일 복사
-shutil.copy(hymn_ppt_path + "\\" + hymn_file1, target_ppt_path + "\\" + hymn_file1)
-shutil.copy(hymn_ppt_path + "\\" + hymn_file2, target_ppt_path + "\\" + hymn_file2)
-shutil.copy(hymn_ppt_path + "\\" + hymn_file3, target_ppt_path + "\\" + hymn_file3)
-shutil.copy(hymn_ppt_path + "\\" + hymn_file4, target_ppt_path + "\\" + hymn_file4)
-shutil.copy(hymn_ppt_path + "\\" + hymn_file5, target_ppt_path + "\\" + hymn_file5)
-shutil.copy(hymn_ppt_path + "\\" + hymn_file6, target_ppt_path + "\\" + hymn_file6)
+# hymn_list.txt파일에서 찬송가 제목 끝에 '\n'이 포함되어 있음
+while True:
+    hymn = hymn_list.readline()
 
+    if( hymn.find(hymn_file1) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file2) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file3) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file4) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file5) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file6) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
 
-# 교독문 파일 복사
-shutil.copy(versicle_ppt_path + "\\" + versicle_file, target_ppt_path + "\\" + versicle_file)
+    if not hymn:
+        break
+
+print(hymn_queue)
