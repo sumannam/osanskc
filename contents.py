45937d2693b04669e97e15419b9790c0b9f599db
osanskc <sumannam@gmail.com>
2021-06-06 14:44:45 +0900

    자막 ppt 파일 생성

--- contents.py
+++ contents.py
@@ -1,5 +1,5 @@
-hymn_path = "E:\\git\\osanskc\\"
-today_ppt_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\2021\\21.05.31"
+hymn_path = "D:\\git\\osanskc\\"
+today_ppt_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\2021\\21.06.06"
 
 # 예배 PPT
 ppt_origin_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\ogrinal_ppt"
@@ -11,12 +11,30 @@ worship_ppt_file4 = "2-1. 자막.pptx"
 # 주보 찬송가들
 hymn_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새찬송가"
 hymn_file1 = "001"
-hymn_file2 = "438"
-hymn_file3 = "536"
-hymn_file4 = "384"
-hymn_file5 = "569"
+hymn_file2 = "070"
+hymn_file3 = "185"
+hymn_file4 = "391"
+hymn_file5 = "585"
 hymn_file6 = "635"
 
 #교도문
 versicle_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새 교독문"
-versicle_file = "095"
+versicle_file = "135"
+
+
+# Slide  3:  1. 묵도
+# Slide  4:  2. 찬송1
+# Slide  5:  2. 찬송2
+# Slide  6:  3. 성시교독
+# Slide  7:  4. 신앙고백
+# Slide  8:  5. 찬송
+# Slide  9:  6. 기도
+# Slide 10:  7. 성경봉독
+# Slide 11:  8. 찬송
+# Slide 12:  9. 설교
+# Slide 12: 10. 설교
+# Slide 13: 11. 헌금
+# Slide 14: 11. 헌금기도
+# Slide 14: 12. 교회소식
+# Slide 15: 13. 찬송
+# Slide 17: 14. 축도
