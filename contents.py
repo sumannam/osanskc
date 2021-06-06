aae0460bd6bc6586a1352fb25fe04db6cef05af2
smnam <sumannam@gmail.com>
2021-06-06 09:58:23 +0900

    낮예배 공통 변수 추가(contents.py)

---
+++ contents.py
@@ -0,0 +1,22 @@
+hymn_path = "E:\\git\\osanskc\\"
+today_ppt_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\2021\\21.05.31"
+
+# 예배 PPT
+ppt_origin_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\ogrinal_ppt"
+worship_ppt_file1 = "1. 주일낮예배.pptx"
+worship_ppt_file2 = "1-1. 자막.pptx"
+worship_ppt_file3 = "2. 주일오후예배.pptx"
+worship_ppt_file4 = "2-1. 자막.pptx"
+
+# 주보 찬송가들
+hymn_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새찬송가"
+hymn_file1 = "001"
+hymn_file2 = "438"
+hymn_file3 = "536"
+hymn_file4 = "384"
+hymn_file5 = "569"
+hymn_file6 = "635"
+
+#교도문
+versicle_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새 교독문"
+versicle_file = "095"
