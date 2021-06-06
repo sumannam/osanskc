fdf2bb0d1edc5580ffe37dd903aaee3421c84152
smnam <sumannam@gmail.com>
2021-06-06 09:59:02 +0900

    순서 공통병수 사용

--- caption_ppt.py
+++ caption_ppt.py
@@ -1,15 +1,50 @@
 from pptx import Presentation
+from contents import *
 
-prs = Presentation('caption.pptx')
-title_slide_layout = prs.slide_layouts[0]
-slide = prs.slides[0]
+# prs = Presentation('caption.pptx')
+# title_slide_layout = prs.slide_layouts[0]
+# slide = prs.slides[0]
 
 
 #slide = prs.slides.add_slide(title_slide_layout)
 # title = slide.shapes.title
-subtitle = slide.placeholders[1]
+# subtitle = slide.placeholders[1]
 
 # title.text = "Hello,p World!"
-subtitle.text = "\n python-pptx was here!"
+# subtitle.text = "\n python-pptx was here!"
 
-prs.save('new-caption.pptx')
+# prs.save('new-caption.pptx')
+
+hymn_file = hymn_path + "hymn_list.txt"
+hymn_list = open(hymn_file, 'r')
+
+
+hymn_queue = []
+
+# hymn_list.txt파일에서 찬송가 제목 끝에 '\n'이 포함되어 있음
+while True:
+    hymn = hymn_list.readline()
+
+    if( hymn.find(hymn_file1) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file2) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file3) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file4) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file5) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+    if( hymn.find(hymn_file6) == 0):
+        hymn = hymn.replace("\n", "")
+        hymn_queue.append(hymn)
+
+    if not hymn:
+        break
+
+print(hymn_queue)
--- worship_ppt.py
+++ worship_ppt.py
@@ -3,28 +3,10 @@ import os
 import sys
 import shutil
 
-hymn_path = "E:\\git\\osanskc\\"
-today_ppt_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\2021\\21.05.31"
+#주일 낮예배 순서 저장(content.py)
+from contents import *
 
-# 예배 PPT
-ppt_origin_path = "E:\\OneDrive\\1. 예배\\예배 PPT\\ogrinal_ppt"
-worship_ppt_file1 = "1. 주일낮예배.pptx"
-worship_ppt_file2 = "1-1. 자막.pptx"
-worship_ppt_file3 = "2. 주일오후예배.pptx"
-worship_ppt_file4 = "2-1. 자막.pptx"
 
-# 주보 찬송가들
-hymn_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새찬송가"
-hymn_file1 = "001"
-hymn_file2 = "438"
-hymn_file3 = "536"
-hymn_file4 = "384"
-hymn_file5 = "569"
-hymn_file6 = "635"
-
-#교도문
-versicle_ppt_path = "E:\\OneDrive\\1. 예배\\새 찬송가\\새 교독문"
-versicle_file = "095"
 
 
 def createFolder(directory):
@@ -68,39 +50,6 @@ def copyFiles(target_ppt_path):
     versicle_file_pptx = versicle_file + ".pptx"
     shutil.copy(versicle_ppt_path + "\\" + versicle_file_pptx, target_ppt_path + "\\" + versicle_file_pptx)
 
-# target_ppt_path = createTargetFolder()
-# copyFiles(target_ppt_path)
+target_ppt_path = createTargetFolder()
+copyFiles(target_ppt_path)
 
-hymn_file = hymn_path + "hymn_list.txt"
-hymn_list = open(hymn_file, 'r')
-
-hymn_queue = []
-
-
-# hymn_list.txt파일에서 찬송가 제목 끝에 '\n'이 포함되어 있음
-while True:
-    hymn = hymn_list.readline()
-
-    if( hymn.find(hymn_file1) == 0):
-        hymn = hymn.replace("\n", "")
-        hymn_queue.append(hymn)
-    if( hymn.find(hymn_file2) == 0):
-        hymn = hymn.replace("\n", "")
-        hymn_queue.append(hymn)
-    if( hymn.find(hymn_file3) == 0):
-        hymn = hymn.replace("\n", "")
-        hymn_queue.append(hymn)
-    if( hymn.find(hymn_file4) == 0):
-        hymn = hymn.replace("\n", "")
-        hymn_queue.append(hymn)
-    if( hymn.find(hymn_file5) == 0):
-        hymn = hymn.replace("\n", "")
-        hymn_queue.append(hymn)
-    if( hymn.find(hymn_file6) == 0):
-        hymn = hymn.replace("\n", "")
-        hymn_queue.append(hymn)
-
-    if not hymn:
-        break
-
-print(hymn_queue)
